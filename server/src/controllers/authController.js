c7aee11659054cc47967f062ec35cca733a25c68
Lucas <lucas.fuentes@student.vinci.be>
2026-02-08 20:35:16 +0100

    [BUG]

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -29,15 +29,19 @@ export const login = async (req, res) => {
     try {
         const user = await prisma.user.findUnique({ where: { name } });
         if (!user) return res.status(404).json({ error: "incorrect name/password" });
+        console.log('user ', user)
 
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) return res.status(401).json({ error: "incorrect name/password" });
+        console.log('isMatch ', isMatch)
 
         const token = jwt.sign(
             { userId: user.id }, 
             process.env.JWT_SECRET, 
             { expiresIn: '24h' }
         );
+
+        console.log('token ', token)
 
         res.status(200).json({
             token, 
