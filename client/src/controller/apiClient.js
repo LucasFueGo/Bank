bd109dd62bb572696baeed735d952c555bba2bce
Lucas <lucas.fuentes@student.vinci.be>
2026-02-08 19:49:02 +0100

    [RF] Generic url

--- client/src/controller/apiClient.js
+++ client/src/controller/apiClient.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+
 const apiClient = axios.create({
-    baseURL: 'http://localhost:3000/api', // url backend
+    baseURL: baseURL,
     withCredentials: true
 });
 
